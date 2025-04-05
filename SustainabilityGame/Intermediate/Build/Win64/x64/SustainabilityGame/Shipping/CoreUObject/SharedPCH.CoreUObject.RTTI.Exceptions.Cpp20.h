cdc59089ba7c2b658711b7d7eb26ba2a3b59656b
D00273016 <D00273016@student.dkit.ie>
2025-04-05 03:05:19 +0100

    Final Score logic added and textures behind the choices
    - Facts about the dresses, shop and the impact to be researched and added

---
+++ SustainabilityGame/Intermediate/Build/Win64/x64/SustainabilityGame/Shipping/CoreUObject/SharedPCH.CoreUObject.RTTI.Exceptions.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "C:/Users/Paulina/Documents/GitHub/SustainabilityGame/SustainabilityGame/Intermediate/Build/Win64/x64/SustainabilityGame/Shipping/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Exceptions.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
